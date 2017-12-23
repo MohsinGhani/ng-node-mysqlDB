1b6561d983f1c00afcbc9018a722152b73badef4
mohsinghani <mohsinghani.777@gmail.com>
2017-12-23 08:38:00 -0800

    working on add collector form

--- src/app/add-doctor/add-doctor.component.ts
+++ src/app/add-doctor/add-doctor.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router/src/config';
+
+interface Collector{
+  name:String,
+  email:String,
+  address:String,
+  salary:Number,
+  qualification:String,
+  dob:String,
+  age:Number,
+  contact:String,
+  gender:String,
+  branchName:String
+  password: String,
+}
 
 @Component({
   selector: 'app-add-doctor',
@@ -6,7 +21,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-doctor.component.scss']
 })
 export class AddDoctorComponent implements OnInit {
-
+  collector: Collector = { name:'', email:'', address:'', salary:0, qualification:'', dob:'', age:0, contact:'', gender:'', branchName:'', password: '',
+  };
   constructor() { }
 
   ngOnInit() {
--- src/app/app.routers.ts
+++ src/app/app.routers.ts
@@ -5,12 +5,20 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'
 import { AdminLoginComponent } from './admin-login/admin-login.component'
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component'
+import { AddDoctorComponent } from './add-doctor/add-doctor.component'
+import { AddCollectorComponent } from './add-collector/add-collector.component'
+import { DoctorListComponent } from './doctor-list/doctor-list.component'
+import { CollectorListComponent } from './collector-list/collector-list.component'
 
 export const router: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'login', component: LoginComponent },
     {path: 'admin', component: AdminLoginComponent },
     {path: 'admin-dashboard', component: AdminDashboardComponent },
+    {path: 'admin-dashboard/add-doctor', component: AddDoctorComponent },
+    {path: 'admin-dashboard/add-collector', component: AddCollectorComponent },
+    {path: 'admin-dashboard/doctor-list', component: DoctorListComponent },
+    {path: 'admin-dashboard/collector-list', component: CollectorListComponent },
     {path: '**', component: HomeComponent},
 ];
 
